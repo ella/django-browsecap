e4666f7ee3f80e0d6fab3cfa3721dbb4091020b2
Honza Kral <honza.kral@gmail.com>
2009-07-24 11:15:10 +0200

    Some comments in code

--- browsecap/browser.py
+++ browsecap/browser.py
@@ -21,12 +21,14 @@ class MobileBrowserParser(object):
         self.parse()
 
     def parse(self):
+        # try egtting the parsed definitions from cache
         data = cache.get(CACHE_KEY)
         if data:
             self.mobile_browsers = map(re.compile, data['mobile_browsers'])
             self.crawlers = map(re.compile, data['crawlers'])
             return
 
+        # parse browscap.ini
         cfg = ConfigParser()
         files = ("browscap.ini", "bupdate.ini")
         read_ok = cfg.read([os.path.join(BC_PATH, name) for name in files])
@@ -37,6 +39,7 @@ class MobileBrowserParser(object):
         browsers = {}
         parents = set()
 
+        # go through all the browsers and record their parents
         for name in cfg.sections():
             sec = dict(cfg.items(name))
             p = sec.get("parent")
@@ -47,29 +50,36 @@ class MobileBrowserParser(object):
         self.mobile_browsers = []
         self.crawlers = []
         for name, conf in browsers.items():
+            # only process those that are not parents - not abstract
             if name not in parents:
                 p = conf.get('parent')
                 if p:
+                    # update config based on parent's settings
                     parent = browsers[p]
                     conf.update(parent)
 
+                # we only care for mobiles and crawlers
                 if conf.get('ismobiledevice', 'false') == 'true' or conf.get('crawler', 'false') == 'true':
                     qname = re.escape(name)
                     qname = qname.replace("\\?", ".").replace("\\*", ".*?")
                     qname = "^%s$" % qname
 
+                # register the user agent
                 if conf.get('ismobiledevice', 'false') == 'true':
                     self.mobile_browsers.append(qname)
 
                 if conf.get('crawler', 'false') == 'true':
                     self.crawlers.append(qname)
 
+        # store in cache to speed up next load
         cache.set(CACHE_KEY, {'mobile_browsers': self.mobile_browsers, 'crawlers': self.crawlers}, CACHE_TIMEOUT)
 
+        # compile regexps
         self.mobile_browsers = map(re.compile, self.mobile_browsers)
         self.crawlers = map(re.compile, self.crawlers)
 
     def find_in_list(self, useragent, agent_list, cache):
+        'Check useragent against agent_list of regexps.'
         try:
             return cache[useragent]
         except KeyError, e:
@@ -85,19 +95,24 @@ class MobileBrowserParser(object):
         return out
 
     def is_mobile(self, useragent):
+        'Returns True if the given useragent is a known mobile browser, False otherwise.'
         return self.find_in_list(useragent, self.mobile_browsers, self.mobile_cache)
 
     def is_crawler(self, useragent):
+        'Returns True if the given useragent is a known crawler, False otherwise.'
         return self.find_in_list(useragent, self.crawlers, self.crawler_cache)
 
 
+# instantiate the parser
 browsers = MobileBrowserParser()
 
+# provide access to methods as functions for convenience
 is_mobile = browsers.is_mobile
 is_crawler = browsers.is_crawler
 
 
 def update():
+    'Download new version of browsecap.ini'
     import urllib
     urllib.urlretrieve("http://browsers.garykeith.com/stream.asp?BrowsCapINI",
                        "browscap.ini")
